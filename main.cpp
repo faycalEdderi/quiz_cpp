2d273ad174869facae6ed50132f3c61984ccf837
faycalEdderi <faycmx@live.fr>
2019-10-06 15:04:09 +0200

    ajout system cls et condition de fin

--- main.cpp
+++ main.cpp
@@ -8,23 +8,18 @@ int main()
 
 {   vector<string>stock_answ(0);
     int result(0);
-    string good_answ[] ={"b","b"};
+    int question_nb(1);
+// !! NE PAS LIRE LA LIGNE EN DESSOUS AVANT DE JOUER !!
+    string good_answ[] ={"b","b"}; // (stock les bonnes reponses !)
+//////////////////////////////////////////////////////////////////////
     string user_answ;
-    string question[] = {
-    "1.	Quelle est le nom mascotte de Nintendo moustachu a casquette rouge :",
+    string question[] = { // Tableau quit stock les questions
+    "1.	Quelle est le nom de la mascotte moustachu de Nintendo a casquette rouge :",
     "2.	Qui est le hero principal de la saga The legend of Zelda ? ",
     };
 
-       /* string answer[0][0]= "a. Luigi";
-     answer[0][1]= "b. luigi";
-     answer[0][2]= "c. Yoshi";
 
-     answer[1][0]= "a. Bowser";
-     answer[1][1]= "b. Zelda";
-     answer[1][2]= "c. Link";*/
-
-
-     const string answer[2][3]{
+     const string answer[2][3]{ // Stockage des réponses possibles
         {"a. Luigi", "b. Mario", "c. Yoshi"},
         {"a. Bowser", "b. Zelda", "c. Link"},
 
@@ -40,29 +35,31 @@ int main()
 
             cout << answer[i][j] << endl ;
         }
-        cin >> user_answ ;
+        cin >> user_answ ; // On entre la réponse ici
 
-
-        stock_answ.push_back(user_answ); // On ajoute une valeur a cet element on rajoute 3 elements
-
-
-         cout << "la taille du tableau dynamique est : " << stock_answ.size() << endl;
-
-
-
+        system ("CLS"); // afficher uniquement la derniere question
+        stock_answ.push_back(user_answ); // stock la reponse dans un tableau dynamique
     }
 
     for(int i=0; i<stock_answ.size(); i++){
 
-    cout << "Question " << i <<" : " << endl;
-    cout << "Bonne reponse : " << good_answ[i] << " Votre reponse : " << stock_answ[i] << endl;
+
+    cout << "Question " << question_nb++ <<" : " << endl;
+    cout << " Votre reponse : " << stock_answ[i] << endl;
          if( good_answ[i] == stock_answ[i] ){
 
             result++;
          }
 
     }
-    cout << "votre score : " << result << endl;
+    cout << "votre score de : " << result << "/2" << endl << endl;
+
+    if(result <= 1){
+        cout << "Echec !";
+    }else if( result == 2){
+    cout << "Perfect ! felicitation !";
+
+    }
 
 return 0;
 }
